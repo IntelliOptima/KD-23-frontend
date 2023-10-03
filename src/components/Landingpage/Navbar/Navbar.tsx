4807ede8d663dd85a82c7d9bfdf0da3a1a4050a4
mikk39k1 <mikk39k1@stud.kea.dk>
2023-10-03 15:36:59 +0200

    Navbar
    Started navbar design

--- src/components/Landingpage/Navbar/Navbar.tsx
+++ src/components/Landingpage/Navbar/Navbar.tsx
@@ -1,18 +1,32 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div>
-        <Image
-            src="/kinoxp-logo-text.png"
-            height={80}
-            width={200}
-            alt="logo"
-            />
+    return (
+        <nav>
+            <div className="flex flex-row items-center justify-between py-4 lg:mx-20 md:mx-10 sm:mx-5">
+                <div>
+                    <Link
+                    href="/"
+                    >
+                        <Image
+                            src="/kinoxp-logo-text.png"
+                            height={80}
+                            width={200}
+                            alt="logo"
+                        />
+                    </Link>
+                </div>
+                <div>
+                    <button className="btn-primary">Sign Up</button>
+                </div>
+                
 
-    </div>
-  )
+            </div>
+        </nav>
+
+    )
 }
 
 export default Navbar
