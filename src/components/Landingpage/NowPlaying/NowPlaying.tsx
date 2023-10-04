a0d0c42ea5e432b009d3e58668029600f3fbe9f8
Ingeman366 <oliv366x@stud.kea.dk>
2023-10-04 21:56:16 +0200

    Now playing init with file structure

---
+++ src/components/Landingpage/NowPlaying/NowPlaying.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NowPlaying = () => {
+    return (
+        <div>
+            <p className="font-inter text-center font-black italic text-8xl">CURRENT SHOWS</p>
+        </div>
+
+    );
+};
+
+export default NowPlaying;
