752198f3184082c85a9be29331b76865e8be3c00
Ingeman366 <oliv366x@stud.kea.dk>
2023-10-04 17:35:35 +0200

    All basics done. Just needs refurbishing, CSS and link/picture logic

--- src/components/Landingpage/StandardSwipe/Button/Button.tsx
+++ src/components/Landingpage/StandardSwipe/Button/Button.tsx
@@ -2,8 +2,8 @@
 
 
 const BuyTicketButton = () =>{
-    return (<button className=' absolute  top-80 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-20 rounded'>
-        <span></span>
+    return (<button className=' absolute left-20 top-80 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-20 rounded'>
+        <p>Buy Ticket</p>
     </button>)
 }
 
