aea720cba2b66d182afc6e7f3334cdf117b9c3ca
mikk39k1 <114911882+mikk39k1@users.noreply.github.com>
2023-10-05 09:24:44 +0200

    Admin Dashboard
    Added dynamic functionality for the sidebar and the content on the page

--- src/components/AdminDashboard/AdminDashboard.tsx
+++ src/components/AdminDashboard/AdminDashboard.tsx
@@ -1,17 +1,35 @@
+"use client";
 import React from 'react'
 import AdminDashboardNavbar from './AdminDashboardNavbar/AdminDashboardNavbar'
 import AdminDashboardSidebar from './AdminDashboardSidebar/AdminDashboardSidebar'
 
-const AdminDashboard = () => {
+type AdminDashboardProps = {
+  children?: React.ReactNode;
+};
 
-    
+const AdminDashboard = ({ children }: AdminDashboardProps) => {
+  const [sidebarOpen, setSidebarOpen] = React.useState(true);
+
+  const openSidebar = () => {
+    setSidebarOpen((prev) => !prev);
+  };
 
   return (
-    <div>
-        <AdminDashboardNavbar />
-        <AdminDashboardSidebar />
-    </div>
-  )
-}
+    <div className="flex flex-col">
+      <AdminDashboardNavbar openSidebar={openSidebar} />
 
-export default AdminDashboard
+      <div className="flex">
+        <AdminDashboardSidebar sidebarOpen={sidebarOpen} />
+        <main
+          className={`flex-grow p-4 transition-transform ${
+            sidebarOpen ? 'ml-[250px]' : 'ml-0'
+          }`}
+        >
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
