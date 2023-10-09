3fb34681b167ceeec0b6c07b5dac4c9a6bfd5061
Ingeman366 <oliv366x@stud.kea.dk>
2023-10-09 11:38:19 +0200

    Base for showview

---
+++ src/app/(user-pages)/book-ticket/page.tsx
@@ -0,0 +1,13 @@
+import BookTicket from '@/components/BookTicket/BookTicket';
+import { NextPage } from 'next'
+import React from 'react'
+
+const Landingpage: NextPage = () => {
+  return (
+    <div>
+        <BookTicket />
+    </div>
+  )
+}
+
+export default Landingpage
---
+++ src/components/BookTicket/Seat/Seat.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+type SeatProps = {
+    id: number,
+    priceWeight: number
+};
+
+const Seat = () => {
+    return (
+        <div className=''></div>
+    );
+}
+
+
+export default Seat;
