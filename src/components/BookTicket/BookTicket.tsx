a29c8db3935ae7f17731a8418e3c919e9f35a9c3
Ingeman366 <oliv366x@stud.kea.dk>
2023-10-18 09:25:41 +0200

    Seatings are now flexed correctly, just needs scewing

--- src/components/BookTicket/BookTicket.tsx
+++ src/components/BookTicket/BookTicket.tsx
@@ -43,7 +43,6 @@ const BookTicket = ({ showId, showPrice, theaterId }: BookTicketProp) => {
         return;
       }
       const data = await response.json();
-      console.log(data)
       setBookings(data);
     } catch (error) {
       console.error('Error fetching booking data:', error);
@@ -59,7 +58,6 @@ const BookTicket = ({ showId, showPrice, theaterId }: BookTicketProp) => {
         return;
       }
       const data = await response.json();
-      console.log(data)
       setTheaterData(data);
     } catch (error) {
       console.error('Error fetching theater data:', error);
@@ -94,23 +92,23 @@ const BookTicket = ({ showId, showPrice, theaterId }: BookTicketProp) => {
     return twoDimensionalArray;
   }
 
-  if (!theaterData || !bookings ) {
+  if (!theaterData || !bookings) {
     return <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-50">
-    <div className="flex justify-center">
-      <CirclesWithBar
-        height="100"
-        width="100"
-        color="#fff"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={IsLoading}
-        outerCircleColor="orange"
-        innerCircleColor="red"
-        barColor=""
-        ariaLabel="circles-with-bar-loading"
-      />
+      <div className="flex justify-center">
+        <CirclesWithBar
+          height="100"
+          width="100"
+          color="#fff"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={IsLoading}
+          outerCircleColor="orange"
+          innerCircleColor="red"
+          barColor=""
+          ariaLabel="circles-with-bar-loading"
+        />
+      </div>
     </div>
-  </div>
   }
 
   /*
@@ -119,10 +117,45 @@ const BookTicket = ({ showId, showPrice, theaterId }: BookTicketProp) => {
   */
 
 
-  const rows = theaterData.totalRows;
-  const seatsPerRow = theaterData.seatsPerRow;
+
+
+
+  const theaterRows = theaterData.totalRows;
+  const theaterSeatsPerRow = theaterData.seatsPerRow;
   const seats = theaterData.seats;
-  const seatArray = convertToTwoDimensionalArray(seats, rows, seatsPerRow);
+
+  const seatArray = convertToTwoDimensionalArray(seats, theaterRows, theaterSeatsPerRow);
+  const leftSideSeatArray = convertLeftSideSeatArray(seats, theaterRows, theaterSeatsPerRow)
+  const rightSideSeatArray = convertRightSideSeatArray(seats, theaterRows, theaterSeatsPerRow)
+
+  function convertLeftSideSeatArray(oneDimensionalArray: Seat[], rows: number, seatsPerRow: number) {
+    const leftArray: Seat[][] = [];
+    for (let row = 0; row < theaterRows; row++) {
+      const oneRowArray = [];
+      for (let seat = 0; seat < seatsPerRow / 2; seat++) {
+        const index = row * seatsPerRow + seat;
+        oneRowArray.push(oneDimensionalArray[index])
+      }
+      leftArray.push(oneRowArray);
+    }
+    return leftArray;
+  }
+
+  function convertRightSideSeatArray(oneDimensionalArray: Seat[], rows: number, seatsPerRow: number) {
+    const rightArray: Seat[][] = [];
+    for (let row = 0; row < theaterRows; row++) {
+      const oneRowArray = [];
+      for (let seat = 5; seat < seatsPerRow; seat++) {
+        const index = row * seatsPerRow + seat;
+        oneRowArray.push(oneDimensionalArray[index])
+      }
+      rightArray.push(oneRowArray);
+    }
+    return rightArray;
+  }
+
+  //const rightSideSeatArray;
+  console.log(leftSideSeatArray)
 
 
   const createBookingFetch = async (email: string) => {
@@ -168,6 +201,7 @@ const BookTicket = ({ showId, showPrice, theaterId }: BookTicketProp) => {
   };
 
 
+
   const toggleSeatSelection = (seat: Seat) => {
     selectedSeats.includes(seat) ? setSelectedSeats(selectedSeats.filter((selectedSeat) => selectedSeat !== seat))
       : setSelectedSeats([...selectedSeats, seat]);
@@ -183,8 +217,10 @@ const BookTicket = ({ showId, showPrice, theaterId }: BookTicketProp) => {
 
   function generateSeats() {
     const seatElements: JSX.Element[] = [];
-    for (let rows = 0; rows < seatArray.length; rows++) {
-      for (let columns = 0; columns < seatArray[rows].length; columns++) {
+
+    for (let rows = 0; rows < theaterRows; rows++) {
+      for (let columns = 0; columns < theaterSeatsPerRow; columns++) {
+
         const currentSeat = seatArray[rows][columns];
         seatElements.push(
           (currentSeat.id !== undefined && !isSeatBooked(currentSeat.id)) ?
@@ -235,8 +271,143 @@ const BookTicket = ({ showId, showPrice, theaterId }: BookTicketProp) => {
       router.push('/')
     }
   }
+/*
+  function generateLeftSideSeats() {
+    const leftSideSeatElements: JSX.Element[] = [];
+
+    for (let seats = 0; seats < theaterSeatsPerRow / 2; seats++) {
+      for (let rows = 0; rows < theaterRows; rows++) {
+        const currentSeat = leftSideSeatArray[rows][seats];
+        leftSideSeatElements.push(
+          (currentSeat.id !== undefined && !isSeatBooked(currentSeat.id)) ?
+            <Seats
+              key={currentSeat.id}
+              seat={currentSeat}
+              isSelected={selectedSeats.includes(currentSeat)}
+              onClick={() => toggleSeatSelection(currentSeat)}
+              isBooked={false}
+            /> :
+            <Seats
+              key={currentSeat.id}
+              seat={currentSeat}
+              isBooked={true}
+            />
+        );
+      }
+    }
+    console.log(leftSideSeatElements)
+    return leftSideSeatElements;
+  }
 
 
+  */
+
+
+  function generateLeftSideSeats() {
+    const leftSideSeatElements: JSX.Element[] = [];
+  
+    for (let seats = 0; seats < theaterSeatsPerRow / 2; seats++) {
+      // Create a div for each column of seats
+      const columnDiv = (
+        <div key={seats} className={`flex flex-col mx-1`}>
+          
+          {Array.from({ length: theaterRows }, (_, rows) => {
+            const currentSeat = leftSideSeatArray[rows][seats];
+            return (
+              <div key={currentSeat.id} className={`seat-wrapper h-1/${theaterRows}`}>
+                {(currentSeat.id !== undefined && !isSeatBooked(currentSeat.id)) ? (
+                  <Seats
+                    seat={currentSeat}
+                    isSelected={selectedSeats.includes(currentSeat)}
+                    onClick={() => toggleSeatSelection(currentSeat)}
+                    isBooked={false}
+                  />
+                ) : (
+                  <Seats
+                    seat={currentSeat}
+                    isBooked={true}
+                  />
+                )}
+              </div>
+            );
+          })}
+        </div>
+      );
+  
+      leftSideSeatElements.push(columnDiv);
+    }
+  
+    console.log(leftSideSeatElements);
+    return leftSideSeatElements;
+  }
+  
+  function generateRightSideSeats() {
+    const rightSideSeatElements: JSX.Element[] = [];
+  
+    for (let seats = 0; seats < theaterSeatsPerRow / 2; seats++) {
+    
+      const columnDiv = (
+        <div key={seats} className={`flex flex-col mx-1`}>
+          
+          {Array.from({ length: theaterRows }, (_, rows) => {
+            const currentSeat = rightSideSeatArray[rows][seats];
+            return (
+              <div key={currentSeat.id} className={`seat-wrapper h-1/${theaterRows}`}>
+                {(currentSeat.id !== undefined && !isSeatBooked(currentSeat.id)) ? (
+                  <Seats
+                    seat={currentSeat}
+                    isSelected={selectedSeats.includes(currentSeat)}
+                    onClick={() => toggleSeatSelection(currentSeat)}
+                    isBooked={false}
+                  />
+                ) : (
+                  <Seats
+                    seat={currentSeat}
+                    isBooked={true}
+                  />
+                )}
+              </div>
+            );
+          })}
+        </div>
+      );
+  
+      rightSideSeatElements.push(columnDiv);
+    }
+  
+    return rightSideSeatElements;
+  }
+
+
+/*
+
+  function generateRightSideSeats() {
+    const rightSideSeatElements: JSX.Element[] = [];
+
+    for (let seats = 0; seats < theaterSeatsPerRow / 2; seats++) {
+      for (let rows = 0; rows < theaterRows; rows++) {
+
+        const currentSeat = rightSideSeatArray[seats][seats];
+        rightSideSeatElements.push(
+          (currentSeat.id !== undefined && !isSeatBooked(currentSeat.id)) ?
+            <Seats
+              key={currentSeat.id}
+              seat={currentSeat}
+              isSelected={selectedSeats.includes(currentSeat)}
+              onClick={() => toggleSeatSelection(currentSeat)}
+              isBooked={false}
+            /> :
+            <Seats
+              key={currentSeat.id}
+              seat={currentSeat}
+              isBooked={true}
+            />
+        );
+      }
+    }
+    return rightSideSeatElements;
+  }
+*/
   return (
     <>
 
@@ -283,11 +454,16 @@ const BookTicket = ({ showId, showPrice, theaterId }: BookTicketProp) => {
             Book selected seats</button>
         </div>
         <div className="theatre flex flex-row items-center justify-center h-screen">
-          <div className="w-[20%] grid grid-cols-10">{generateSeats()}</div>
+          <div className="flex flex-row items-center justify-center mr-3">
+            {generateLeftSideSeats()}
+          </div>
+          <div className="flex flex-row items-center justify-center ml-3">
+            {generateRightSideSeats()}
+          </div>
         </div>
       </div>
     </>
   );
 };
-
+// <div className="w-[20%] grid grid-cols-10">{generateSeats()}</div>
 export default BookTicket;
--- src/components/BookTicket/Seats.tsx
+++ src/components/BookTicket/Seats.tsx
@@ -14,16 +14,25 @@ type SeatProp = {
 const Seats = ({ seat, onClick, isSelected, isBooked } :SeatProp ) => {
 
 
-const notBooked = `mb-2 w-8 h-12 rounded-md hover:bg-sky-800 hover:cursor-pointer ${
-    isSelected ? 'bg-rose-300' : 'bg-gradient-to-t from-red-900 via-rose-900 to-red-400'
+  const notBooked = `-mb-4 w-8 h-12 rounded-md hover:cursor-pointer transition-all duration-500 ${
+    !isSelected
+      ? 'bg-gradient-to-t from-red-900 via-rose-900 to-red-400'
+      : 'bg-rose-300'
   }`;
+  
 
+
+/*
+const notBooked = `mb-2 w-8 h-12 rounded-md hover:cursor-pointer ${
+    isSelected ? 'bg-rose-300' : 'bg-gradient-to-t from-red-900 via-rose-900 to-red-400 hover:bg-sky-800'
+  }`;
+*/
 /*
   const notBooked = `mb-2 w-6 h-8 rounded-t-lg hover:bg-orange-300 hover:cursor-pointer ${
     isSelected ? 'bg-blue-500' : 'bg-green-500'
   }`; 
 */
-  const booked = `mb-2 w-8 h-12 rounded-md bg-gradient-to-t from-blue-900 via-blue-700 to-blue-400`;
+  const booked = `-mb-4 w-8 h-12 rounded-md bg-gradient-to-t from-blue-900 via-blue-700 to-blue-400`;
 
 
     const handleClick: MouseEventHandler<HTMLDivElement> = (event: React.MouseEvent<HTMLDivElement>) => {
@@ -40,7 +49,7 @@ const notBooked = `mb-2 w-8 h-12 rounded-md hover:bg-sky-800 hover:cursor-pointe
         data-numberinrow={seat.numberInRow}
         className={isBooked ? booked : notBooked}
         onClick={handleClick}
-      />
+        />
     );
   };
 
